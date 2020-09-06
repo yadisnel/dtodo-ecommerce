3209ae823db7caa60147509d3d5e3d77894abe2e
Osiel Lima <osielmesa@gmail.com>
2020-09-06 14:53:43 +0200

    Adding ability to theming

--- web/kupiti/src/App.js
+++ web/kupiti/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { ThemeProvider } from '@material-ui/core/styles';
+
+//Local
 import './App.css';
 import Main from './containers/Main'
+import Theme from './theme/Theme'
 
 function App() {
   return (
-    <Main/>
+    <ThemeProvider theme={Theme}>
+      <Main/>
+    </ThemeProvider>
   );
 }
 
